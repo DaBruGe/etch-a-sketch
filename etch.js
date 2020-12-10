ed61a9003277f606520981f7dda4ccb3c2f05987
Dave Geddes <davidbrucegeddes@gmail.com>
2020-12-10 07:50:12 +0000

    16x16 grid added

--- etch.js
+++ etch.js
@@ -1,6 +1,9 @@
 const grid = document.querySelector('#grid');
 
 for (let i = 0; i < 16; i++) {
-    const div = document.createElement('div');
-    grid.appendChild(div);
+    for (let j = 0; j < 16; j++) {
+        const div = document.createElement('div');
+        div.classList.add("cell");
+        grid.appendChild(div);
+    }
 }
