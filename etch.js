e3e28307f9b6b7bdaa442ffb0fe97f145b81b0c5
Dave Geddes <davidbrucegeddes@gmail.com>
2021-01-14 07:45:25 +0000

    added colour change button functionality

--- etch.js
+++ etch.js
@@ -1,29 +1,41 @@
 let gridSize = 16;
+let fillMode = "black";
 const grid = document.querySelector('#grid');
 
-generateGrid(gridSize);
+generateGrid(gridSize, fillMode);
+
+const blackFill = document.querySelector('#blackFill')
+blackFill.addEventListener('click', () => {
+    fillMode = "black";
+    cellListener(fillMode);
+});
+
+const rainbowFill = document.querySelector('#rainbowFill')
+rainbowFill.addEventListener('click', () => {
+    fillMode = "rainbow";
+    cellListener(fillMode);
+});
+
+const greyscaleFill = document.querySelector('#greyscaleFill')
+greyscaleFill.addEventListener('click', () => {
+    fillMode = "greyscale";
+    cellListener(fillMode);
+});
+
+
 
 const cells = document.querySelectorAll('.cell');
 
-cellListener();
-
-
-
 const reset = document.querySelector('#clearGrid')
-
 reset.addEventListener('click', () => {
     clearGrid(cells);
     do {
         gridSize = prompt("Select size of new grid. Maximum 100.")
     } while (gridSize > 100);
-    generateGrid(gridSize);
+    generateGrid(gridSize, fillMode);
 });
 
 
-
-function cellToBlack() {
-    this.style.backgroundColor = "black";
-}
 
 function clearGrid(cells) {
     cells = document.querySelectorAll('.cell');
@@ -32,7 +44,7 @@ function clearGrid(cells) {
     })
 }
 
-function generateGrid(gridSize) {
+function generateGrid(gridSize, fillMode) {
     
     grid.style.setProperty('--grid-rows', gridSize);
     grid.style.setProperty('--grid-cols', gridSize);
@@ -42,16 +54,32 @@ function generateGrid(gridSize) {
         div.classList.add("cell");
         grid.appendChild(div);
     }
-    cellListener();
+    cellListener(fillMode);
 }
 
-function cellListener() {
+function cellListener(fillMode) {
     const cells = document.querySelectorAll('.cell');
 
     cells.forEach((cell) => {
-    
-        cell.addEventListener('mouseover', cellToBlack);
+        cell.removeEventListener('mouseover', cellToBlack);
+        cell.removeEventListener('mouseover', cellToRainbow);
+        cell.removeEventListener('mouseover', cellToGreyscale);
     });
+    
+    cells.forEach((cell) => {
+        if (fillMode === "black") {
+            cell.addEventListener('mouseover', cellToBlack);
+        } else if (fillMode === "rainbow") {
+            cell.addEventListener('mouseover', cellToRainbow);
+        } else if (fillMode === "greyscale") {
+            cell.addEventListener('mouseover', cellToGreyscale);
+        }
+    });
+}
+
+
+function cellToBlack() {
+    this.style.backgroundColor = "black";
 }
 
 function cellToRainbow() {
@@ -60,4 +88,11 @@ function cellToRainbow() {
     let blue = Math.floor(Math.random() * 255);
     let col = "rgb(" + red + "," + green + "," + blue + ")";
     this.style.backgroundColor = col;
+}
+
+function cellToGreyscale() {
+    let shade = this.style.opacity;
+    if (shade < 1) {
+        this.style.opacity = shade + 0.1;
+    }
 }
